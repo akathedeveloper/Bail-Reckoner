82d85b0eff022382c74945aab88496c55791ef6d
gautam-divyanshu <gautamdivyanshu034@gmail.com>
2025-02-13 22:42:50 +0530

    basic components and setup

---
+++ frontend/src/pages/BailPage.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import BailEligibility from "../components/BailEligibility";
+
+const BailPage = () => {
+  return (
+    <div className="bail-page">
+      <Navbar />
+      <BailEligibility />
+    </div>
+  );
+};
+
+export default BailPage;
